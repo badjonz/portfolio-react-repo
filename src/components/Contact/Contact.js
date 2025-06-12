5794ce2291807c06933fe0b954525d39d8cd4704
Jon Kumar <badjonz@gmail.com>
2025-06-12 22:05:11 +0900

    Made some changes to test the netlify submit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,35 @@
+import React, { useState } from 'react';
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        'form-name': form.getAttribute('name'),
+        ...formData,
+      }).toString(),
+    })
+      .then(() => alert('Form submitted successfully!'))
+      .catch((error) => alert(error));
+  };
+
   return (
     <section className='section-contact' id='section-contact'>
       <div className='row'>
@@ -6,12 +37,13 @@ const Contact = () => {
           <div className='overlay'>&nbsp;</div>
           <div className='contact__form'>
             <form
-              name='contact'
+              name='portfolioContact'
               className='form'
               method='POST'
               data-netlify='true'
-              onSubmit='submit'
+              onSubmit={handleSubmit}
             >
+              <input type='hidden' name='form-name' value='portfolioContact' />
               <div className='u-margin-bottom-medium'>
                 <h2 className='heading-secondary'>
                   You can hire me<span className='fullstop'>.</span>
@@ -25,6 +57,8 @@ const Contact = () => {
                   required
                   id='name'
                   name='name'
+                  value={formData.name}
+                  onChange={handleChange}
                 />
                 <label htmlFor='name' className='form__label'>
                   Full name
@@ -38,6 +72,8 @@ const Contact = () => {
                   required
                   id='email'
                   name='email'
+                  value={formData.email}
+                  onChange={handleChange}
                 />
                 <label htmlFor='email' className='form__label'>
                   Email address
@@ -50,6 +86,8 @@ const Contact = () => {
                   placeholder='Subject'
                   id='subject'
                   name='subject'
+                  value={formData.subject}
+                  onChange={handleChange}
                 />
                 <label htmlFor='subject' className='form__label'>
                   Subject
@@ -63,6 +101,8 @@ const Contact = () => {
                   cols='30'
                   rows='6'
                   placeholder='Message'
+                  value={formData.message}
+                  onChange={handleChange}
                   required
                 ></textarea>
 
@@ -70,12 +110,14 @@ const Contact = () => {
                   Message
                 </label>
               </div>
-              <div className='form__group'>
+              {/* <div className='form__group'>
                 <div data-netlify-recaptcha='true'></div>
-              </div>
+              </div> */}
 
               <div className='form__group'>
-                <button className='btn'>Send message &rarr;</button>
+                <button className='btn' type='submit'>
+                  Send message &rarr;
+                </button>
               </div>
             </form>
           </div>
